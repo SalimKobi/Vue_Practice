b4c655835ae858de01fd127b40cb01fb4afb2823
SalimKobi <salimkobi9@gmail.com>
2021-10-31 14:42:20 +0100

    added a function

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ var app = new Vue ({
         image: 'assets/vmSocks-green-onWhite.jpeg',
         link: 'https://kippa.africa/',
         inventory: 8,
-        details: ["80% cotton", "20% polyster","Gender Neutral" ]
+        details: ["80% cotton", "20% polyster","Gender Neutral" ],
+        variants:[{variantId: 2234,
+        variantColor: "green"},{variantId: 2235,
+        variantColor: "blue"},],
+        cart: 0,
+        methods: {
+            addToCart: function () {
+                this.cart += 1
+            }
+        }
     }
 });
